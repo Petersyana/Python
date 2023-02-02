eec3e2f07b469c0130610dc5a77dd26956e8354b
Yana <petersyana@rambler.ru>
2023-02-02 19:53:09 +0300

    add correction task4_5.py

--- Seminar4/task4_5.py
+++ Seminar4/task4_5.py
@@ -3,23 +3,17 @@
 с помощью модуля timeit, опишите результат
 """
 
-# С использованием функции sort() время выполнения быстрее
-
 # используя функцию sort()
-from timeit import timeit  # без sort()   0.13229730003513396
-from timeit import timeit  # sort()   0.10654509998857975
+from timeit import timeit
 
-print(timeit("""
-def my_func(a, b, c = 0):
+def my_func_sort(a, b, c = 0):
     list_1 = [a,b,c]
     list_1.sort()
     summa = b + c
     return summa
-    """))
-
+   
 # без функции sort()
 
-print(timeit("""
 def my_func(a, b, c = 0):
     summa = 0
     if a > b:
@@ -35,4 +29,7 @@ def my_func(a, b, c = 0):
         else:
             summa = summa + c
     return summa
-    """))
+
+print('Время выполнения функций')
+print('my_func_sort :', timeit("my_func_sort(3,4,6)", globals=globals(), number=10000))
+print('my_func :', timeit("my_func(3,4,6)", globals=globals(), number=10000))
