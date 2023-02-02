779eea6a3b2d8c9ab2ad6612d8c9a99a2d5afc59
Yana <petersyana@rambler.ru>
2023-02-02 19:58:42 +0300

    add correction1 task4_5.py

--- Seminar4/task4_5.py
+++ Seminar4/task4_5.py
@@ -6,15 +6,17 @@
 # используя функцию sort()
 from timeit import timeit
 
-def my_func_sort(a, b, c = 0):
-    list_1 = [a,b,c]
+
+def my_func_sort(a, b, c=0):
+    list_1 = [a, b, c]
     list_1.sort()
     summa = b + c
     return summa
-   
+
 # без функции sort()
 
-def my_func(a, b, c = 0):
+
+def my_func(a, b, c=0):
     summa = 0
     if a > b:
         summa = summa + a
@@ -30,6 +32,8 @@ def my_func(a, b, c = 0):
             summa = summa + c
     return summa
 
+
 print('Время выполнения функций')
-print('my_func_sort :', timeit("my_func_sort(3,4,6)", globals=globals(), number=10000))
+print('my_func_sort :', timeit(
+    "my_func_sort(3,4,6)", globals=globals(), number=10000))
 print('my_func :', timeit("my_func(3,4,6)", globals=globals(), number=10000))
