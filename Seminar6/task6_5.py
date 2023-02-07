f5af42661a8fed77b0e9d7a5d508a8d9a877d280
Yana <petersyana@rambler.ru>
2023-02-07 17:45:41 +0300

    add task6_5.py

--- Seminar6/task6_5.py
+++ Seminar6/task6_5.py
@@ -7,8 +7,7 @@
 """
 
 from functools import reduce
-my_list = [el for el in range(100,1001) if el % 2 == 0]
+my_list = [el for el in range(100, 1001) if el % 2 == 0]
 
 multiplication = reduce((lambda x, y: x * y), my_list)
 print(multiplication)
-
